7ec2a66866cb3d29d166ae3500d3028193eb5b34
Himadri Talukder <htalukder@ibm.com>
2024-03-14 09:01:20 -0500

    updated missing reference

--- code/main.py
+++ code/main.py
@@ -97,7 +97,7 @@ elif(component=="inference"):
     finetuned_model = config['Inference']['finetuned_model']
     input_dataset = config['Inference']['input_dataset']
     
-    inf_response = inf.funcInference(exp_name,model_name,finetuned_model,input_dataset)
+    inf_response = inf.executeInference(config)
     print(inf_response)
     
 elif(component=="querycorrection"):
